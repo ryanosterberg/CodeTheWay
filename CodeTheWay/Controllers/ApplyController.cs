5cc01befdd689cd7327f127448c4ddd6f2f5154d
MohnishKalia <mohnish.kalia@gmail.com>
2019-11-11 12:42:42 -0600

    Apply CSS and add apply controller

---
+++ CodeTheWay/Controllers/ApplyController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTheWay.Controllers
+{
+    public class ApplyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("GetInvolved");
+        }
+
+        public ActionResult GetInvolved()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    }
+}
