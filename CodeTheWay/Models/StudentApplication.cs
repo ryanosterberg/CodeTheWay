d6063a63abed4456d9857d8b007381effb22a910
MohnishKalia <mohnish.kalia@gmail.com>
2019-11-15 17:00:10 -0600

    EmailAddress annotation

--- CodeTheWay/Models/StudentApplication.cs
+++ CodeTheWay/Models/StudentApplication.cs
@@ -14,33 +14,43 @@ namespace CodeTheWay.Models
         [DisplayName("First Name")]
         [Required]
         public string FirstName { get; set; }
+
         [DisplayName("Last Name")]
         [Required]
         public string LastName { get; set; }
-        [DataType(DataType.EmailAddress)]
+
+        [EmailAddress]
         [Required]
-        //[RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
         [DisplayName("High School")]
         [Required]
         public string HighSchool { get; set; }
+
         [DisplayName("Estimated Graduation Date")]
         [DataType(DataType.Date)]
         [Required]
         public DateTime EstGradDate { get; set; }
+
         [DisplayName("Do you have a Windows Laptop?")]
         public bool WindowsLaptop { get; set; }
+
         [DisplayName("Have you completed AP Computer Science or equivalent?")]
         public bool CSAComplete { get; set; }
+
         [DisplayName("If not, please detail the computer science classes you have taken and your computer programming experiences")]
         [DataType(DataType.MultilineText)]
         public string Accomplishments { get; set; }
+
         [DisplayName("Will you be here for all days of the Master Class?")]
         public bool PresentAllClassDates { get; set; }
+
         [DisplayName("If not, list all missed class days")]
         public string MissedClassDates { get; set; }
+
         [DisplayName("Will you be here for all days of the Project Season?")]
         public bool PresentAllSeasonDates { get; set; }
+
         [DisplayName("If not, list all missed season days")]
         public string MissedSeasonDates { get; set; }
     }
