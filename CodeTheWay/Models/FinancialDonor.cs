dd1a2a672a94fb53cf742a0807bd5a3d81d8bb2b
MohnishKalia <mohnish.kalia@gmail.com>
2019-11-19 13:36:30 -0600

    Spacing

--- CodeTheWay/Models/FinancialDonor.cs
+++ CodeTheWay/Models/FinancialDonor.cs
@@ -11,22 +11,28 @@ namespace CodeTheWay.Models
     {
         [Key]
         public int Id { get; set; }
+
         [DisplayName("First Name")]
         [Required]
         public string FirstName { get; set; }
+
         [DisplayName("Last Name")]
         [Required]
         public string LastName { get; set; }
+
         [EmailAddress]
         [Required]
         public string Email { get; set; }
+
         /// <summary>
         /// An enum that represents how often payments are scheduled.
         /// </summary>
         public enum Frequency { BIANNUAL, WEEKLY, MONTHLY, YEARLY, ONCE }
-        [DisplayName("Estamited Ammount")]
+
+        [DisplayName("Estimated Amount")]
         [Required]
         public double EstAmt { get; set; }
+
         public Frequency CurrentFrequency { get; set; }
     }
 }
--- CodeTheWay/Models/VolunteerDonor.cs
+++ CodeTheWay/Models/VolunteerDonor.cs
@@ -11,17 +11,22 @@ namespace CodeTheWay.Models
     {
         [Key]
         public int Id { get; set; }
+
         [DisplayName("First Name")]
         [Required]
         public string FirstName { get; set; }
+
         [DisplayName("Last Name")]
         [Required]
         public string LastName { get; set; }
+
         [EmailAddress]
         [Required]
         public string Email { get; set; }
+
         [Required]
         public string Company { get; set; }
+
         [Required]
         public string Offerings { get; set; }
     }
