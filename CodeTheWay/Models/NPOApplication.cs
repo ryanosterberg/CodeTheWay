433d26cd02ccff963e30de9c741cd386efe9ae0c
Aaron <amelch130@gmail.com>
2019-11-19 11:52:22 -0600

    Update models and data annotation

--- CodeTheWay/Models/NPOApplication.cs
+++ CodeTheWay/Models/NPOApplication.cs
@@ -11,37 +11,53 @@ namespace CodeTheWay.Models
     {
         [Key]
         public int Id { get; set; }
+
         [DisplayName("Organization Name")]
         [Required]
         public string OrgName { get; set; }
+
         [EmailAddress]
         [Required]
         public string Email { get; set; }
+
         [DisplayName("Phone Number")]
         [Phone]
         public string PhoneNum { get; set; }
+
         public string Address { get; set; }
-        [DisplayName("Applicant Name")]
+
+        [DisplayName("Applicant First Name")]
         [Required]
-        public string ApplicantName { get; set; }
+        public string ApplicantFirstName { get; set; }
+
+        [DisplayName("Applicant Last Name")]
+        [Required]
+        public string ApplicantLastName { get; set; }
+
         [DisplayName("Applicant Position")]
         [Required]
         public string ApplicantPosition { get; set; }
+
         [DisplayName("Applicant Email")]
         [EmailAddress]
         public string ApplicantEmail { get; set; }
+
         [DisplayName("Applicant Phone Number")]
         [Phone]
         public string ApplicantPhone { get; set; }
+
         [DisplayName("Misson")]
         [Required]
         public string NPOMission { get; set; }
+
         [DisplayName("Vision")]
         [Required]
         public string NPOVision { get; set; }
+
         [DisplayName("Website URL")]
         [Url]
         public string WebURL { get; set; }
+
         [DisplayName("Problems and Desires")]
         [Required]
         public string ProblemsAndDesires { get; set; }
