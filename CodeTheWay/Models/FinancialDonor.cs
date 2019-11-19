e36e2fd136971b3ecdafaf7545e5fd75e7da5d38
Aaron <amelch130@gmail.com>
2019-11-19 11:33:25 -0600

    Added stuff to the models

--- CodeTheWay/Models/FinancialDonor.cs
+++ CodeTheWay/Models/FinancialDonor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,13 +11,21 @@ namespace CodeTheWay.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("First Name")]
+        [Required]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required]
         public string LastName { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
         /// <summary>
         /// An enum that represents how often payments are scheduled.
         /// </summary>
         public enum Frequency { BIANNUAL, WEEKLY, MONTHLY, YEARLY, ONCE }
+        [DisplayName("Estamited Ammount")]
+        [Required]
         public double EstAmt { get; set; }
         public Frequency CurrentFrequency { get; set; }
     }
--- CodeTheWay/Models/NPOApplication.cs
+++ CodeTheWay/Models/NPOApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,17 +11,39 @@ namespace CodeTheWay.Models
     {
         [Key]
         public int Id { get; set; }
+        [DisplayName("Organization Name")]
+        [Required]
         public string OrgName { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+        [DisplayName("Phone Number")]
+        [Phone]
         public string PhoneNum { get; set; }
         public string Address { get; set; }
+        [DisplayName("Applicant Name")]
+        [Required]
         public string ApplicantName { get; set; }
+        [DisplayName("Applicant Position")]
+        [Required]
         public string ApplicantPosition { get; set; }
+        [DisplayName("Applicant Email")]
+        [EmailAddress]
         public string ApplicantEmail { get; set; }
+        [DisplayName("Applicant Phone Number")]
+        [Phone]
         public string ApplicantPhone { get; set; }
+        [DisplayName("Misson")]
+        [Required]
         public string NPOMission { get; set; }
+        [DisplayName("Vision")]
+        [Required]
         public string NPOVision { get; set; }
+        [DisplayName("Website URL")]
+        [Url]
         public string WebURL { get; set; }
+        [DisplayName("Problems and Desires")]
+        [Required]
         public string ProblemsAndDesires { get; set; }
     }
 }
