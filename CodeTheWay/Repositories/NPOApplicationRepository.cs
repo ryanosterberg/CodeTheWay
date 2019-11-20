0c26839955f5e37ce748410f6d6d5301ec7f3322
Aaron <amelch130@gmail.com>
2019-11-19 21:21:57 -0600

    Added NPO service and repo

---
+++ CodeTheWay/Repositories/NPOApplicationRepository.cs
@@ -0,0 +1,42 @@
+﻿using CodeTheWay.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeTheWay.Repositories
+{
+    public class NPOApplicationRepository
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public List<NPOApplication> GetNPOApplications()
+        {
+            return db.NPOApplications.ToList();
+        }
+
+        public NPOApplication GetNPOApplicationById(int id)
+        {
+            return db.NPOApplications.Find(id);
+        }
+
+        public void Add(NPOApplication toAdd)
+        {
+            db.NPOApplications.Add(toAdd);
+            db.SaveChanges();
+        }
+
+        public void Delete(NPOApplication toDelete)
+        {
+            db.NPOApplications.Remove(toDelete);
+            db.SaveChanges();
+        }
+
+        public void Edit(NPOApplication toEdit)
+        {
+            db.Entry(toEdit).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ CodeTheWay/Services/NPOApplicationService.cs
@@ -0,0 +1,38 @@
+﻿using CodeTheWay.Models;
+using CodeTheWay.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeTheWay.Services
+{
+    public class NPOApplicationService
+    {
+        private NPOApplicationRepository repo = new NPOApplicationRepository();
+        public List<NPOApplication> GetNPOApplications()
+        {
+            return repo.GetNPOApplications();
+        }
+
+        public NPOApplication GetNPOApplicationById(int id)
+        {
+            return repo.GetNPOApplicationById(id);
+        }
+
+        public void Add(NPOApplication toAdd)
+        {
+            repo.Add(toAdd);
+        }
+
+        public void Delete(NPOApplication toDelete)
+        {
+            repo.Delete(toDelete);
+        }
+
+        public void Edit(NPOApplication toEdit)
+        {
+            repo.Edit(toEdit);
+        }
+    }
+}
