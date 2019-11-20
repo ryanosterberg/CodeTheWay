33db6293e2d4a334117c9794353f88c1139ee173
Mohnish Kalia <20kaliam@elmbrookstudents.org>
2019-11-19 19:48:50 -0600

    Working without anti-forgery

--- CodeTheWay/Controllers/FinancialDonorController.cs
+++ CodeTheWay/Controllers/FinancialDonorController.cs
@@ -45,7 +45,6 @@ namespace CodeTheWay.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,EstAmt,CurrentFrequency")] FinancialDonor financialDonor)
         {
             if (ModelState.IsValid)
